5561d0badb79e585dfd8f5abb8e8592eb0eaf019
Chris <chriswndegwa@gmail.com>
2024-11-18 13:16:12 +0300

    final

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
-import "./index.css";
+import App from "./components/App";  // Import App component
+import "./index.css";  // Import global styles
 
+// Render the App component inside the root div element
 ReactDOM.render(<App />, document.getElementById("root"));
